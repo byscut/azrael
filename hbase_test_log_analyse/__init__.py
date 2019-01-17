6d7891ca1825dfbcc956e6b2cfccd1c5334a78dd
Baiyang <baiyang@haizhi.com>
2019-01-17 17:16:34 +0800

    hbase日志分析

---
+++ hbase_test_log_analyse/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/17 16:45
+# @Author  : Azrael.Bai
+# @File    : __init__.py.py
---
+++ hbase_test_log_analyse/hbase_log_file_parser.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/17 16:47
+# @Author  : Azrael.Bai
+# @File    : hbase_log_file_parser.py
+import os
+from logger import AppLogger
+
+def eachFile(filepath):
+    pathDir =  os.listdir(filepath)
+    for allDir in pathDir:
+        child = os.path.join('%s%s' % (filepath, allDir))
+        yield child
+
+if __name__ == '__main__':
+    log = AppLogger('hbase_log.log').get_logger()
+
+    log.info("start analyse ....")
+    for file_name in eachFile('../DATAS/hbase_logoutput/'):
+        with open(file_name, 'r') as pfile:
+            count = 0
+            for line in pfile.readlines():
+                if line.strip().split('\t').__len__() == 2:
+                    sec = line.strip().split('\t')[1]
+                    try:
+                        if int(sec) > 100:
+                            log.info(line.strip())
+                            count += 1
+                    except:
+                        log.exception('some exception')
+            log.info("{} exec long times: {}".format(file_name, count))
