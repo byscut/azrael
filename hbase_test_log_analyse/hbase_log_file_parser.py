faf582b3cbfc37a5eb3e50e83657dfd339817b92
Baiyang <baiyang@haizhi.com>
2019-01-18 10:58:07 +0800

    hbase日志测试修改

--- hbase_test_log_analyse/hbase_log_file_parser.py
+++ hbase_test_log_analyse/hbase_log_file_parser.py
@@ -18,13 +18,18 @@ if __name__ == '__main__':
     for file_name in eachFile('../DATAS/hbase_logoutput/'):
         with open(file_name, 'r') as pfile:
             count = 0
+            max = 0
             for line in pfile.readlines():
                 if line.strip().split('\t').__len__() == 2:
                     sec = line.strip().split('\t')[1]
                     try:
+                        if int(sec) > max:
+                            max = int(sec)
                         if int(sec) > 100:
                             log.info(line.strip())
                             count += 1
                     except:
                         log.exception('some exception')
-            log.info("{} exec long times: {}".format(file_name, count))
+            log.info("{} exec long times: {}, max time is :{}".format(file_name, count, max))
+
+    log.info("end analyse ....")
